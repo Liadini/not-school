1a134da13c4c61c99ff0ba2a060b8f161d748580
Liad Hermelin <liad.hermelin@gmail.com>
2019-01-21 08:41:04 -0800

    finished SentenceGenerator, added GeometryCalculator

---
+++ SentenceGenerator/src/sentencegenerator/SentenceGenerator.java
@@ -0,0 +1,24 @@
+package sentencegenerator;
+
+import java.util.Random;
+
+public class SentenceGenerator {
+
+	public String getShakespeareanInsult() {
+		Random rand = new Random();
+		String[] adjArr = new String[] {"hateful", "nasty", "common", "bloody", "vacant", "knavish", "quarrelsome", 
+				"grotesque", "revolting", "unkind", "saucy", "disgusting", "grizzled", "spotted", "cantankerous", 
+				"lewd", "loathsome", "brazen", "detestable", "reeky", "repugnant", "abominable", "peevish", "greasy"};
+		String[] midArr = new String[] {"ill-breeding", "low-spirited", "cold-blooded", "luggage-lipping", "double-dealing", 
+				"pale-hearted", "sallow-skinned", "puke-stained", "green-eyed", "lily-livered", "eye-offending", "long-winded", 
+				"pasty-faced", "dark-hearted", "low-down", "wax-eared", "onion-breathing", "paste-brained", "broke-dealing", 
+				"dull-spirited", "stone-faced", "black-winged", "dull-witted", "poison-tongued"};
+		String[] nounArr = new String[] {"laughingstock", "twit", "ruffian", "pirate", "buffoon", "boar", "maggot-pie", 
+				"hog", "scoundrel", "dolt", "jack-a-nape", "serpent", "dullard", "devil", "dung-hill", "fool", "coward", 
+				"puke-stocking", "leper", "skainsmate", "infection", "simpleton", "villain", "miscreant"};
+		int adjNum = rand.nextInt(adjArr.length);
+		int midNum = rand.nextInt(midArr.length);
+		int nounNum = rand.nextInt(nounArr.length);
+		return "Thou art a " + adjArr[adjNum] + " " + midArr[midNum] + " " + nounArr[nounNum] + "!";
+	}
+}
---
+++ SentenceGenerator/src/sentencegenerator/SentenceGeneratorMenu.java
@@ -0,0 +1,42 @@
+package sentencegenerator;
+
+import java.util.Scanner;
+
+public class SentenceGeneratorMenu {
+
+	public static void main(String[] args) throws InterruptedException {
+		SentenceGenerator generator = new SentenceGenerator();
+		Scanner sc = new Scanner(System.in);
+		boolean shouldContinue = true;
+		do {
+			System.out.println("1) Shakespearean Insult Generator!");
+			System.out.println("2) Random Sentence Generator (in progress)!");
+			System.out.println("3) Quit!");
+			System.out.print("Choose an option ---> ");
+			String choice = sc.next();
+			switch (choice) {
+			case "1":
+			case "shakespeare":
+			case "Shakespeare":
+				System.out.println(generator.getShakespeareanInsult() + "\n");
+				Thread.sleep(2000);
+				break;
+			case "2":
+			case "sentence":
+			case "Sentence":
+				System.out.println("I'm sorry, but this menu option is a work in progress. Please pick another! \n");
+				Thread.sleep(2000);
+				break;
+			case "3":
+			case "quit":
+				System.out.println("Thanks for playing!");
+				sc.close();
+				shouldContinue = false;
+				break;
+			default:
+				System.out.println("I'm sorry, but that menu option doesn't exist. Please pick another! \n");
+				Thread.sleep(2000);
+			}
+		} while (shouldContinue);
+	}
+}
