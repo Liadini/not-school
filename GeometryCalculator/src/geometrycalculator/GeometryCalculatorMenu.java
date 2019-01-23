ab93e6699e3674ec699582883a27443f1ab93bb8
Liad Hermelin <liad.hermelin@gmail.com>
2019-01-22 20:13:04 -0800

    added various features to GeometryCalculator

--- GeometryCalculator/src/geometrycalculator/GeometryCalculatorMenu.java
+++ GeometryCalculator/src/geometrycalculator/GeometryCalculatorMenu.java
@@ -10,7 +10,7 @@ public class GeometryCalculatorMenu {
 		GeometryCalculator calc = new GeometryCalculator();
 		Scanner sc = new Scanner(System.in);
 		double length1, length2, length3, length4, length5, length6, length7, length8;
-		boolean shouldContinue = true, shouldRepeat = false;
+		boolean shouldContinue = true;
 		do {
 			System.out.print("Enter a shape! ---> ");
 			String str = sc.next();
@@ -21,92 +21,28 @@ public class GeometryCalculatorMenu {
 			case "SQ":
 				System.out.print("Enter a side length ---> ");
 				length1 = sc.nextDouble();
-				System.out.print("Would you like to find the area or perimeter of the square? ---> ");
-				do {
-					switch (sc.next().toUpperCase()) {
-					case "AREA":
-					case "AR":
-					case "A":
-						System.out.println("Area of square = " + calc.squareArea(length1) + "\n");
-						Thread.sleep(2000);
-						shouldRepeat = false;
-						break;
-					case "PERIMETER":
-					case "PER":
-					case "P":
-						System.out.println("Perimeter of square = " + calc.squarePerimeter(length1) + "\n");
-						Thread.sleep(2000);
-						shouldRepeat = false;
-						break;
-					default:
-						System.out.println("I'm sorry, that wasn't an option! Please try again.\n");
-						Thread.sleep(1000);
-						shouldRepeat = true;
-						break;
-					}
-				} while (shouldRepeat);
+				System.out.println("Area of square = " + calc.squareArea(length1) + 
+						", Perimeter of square = " + calc.squarePerimeter(length1) + "\n");
+				Thread.sleep(2000);
 				break;
 			case "RECTANGLE":
 			case "RECT":
 				System.out.print("Enter the first side length ---> ");
 				length1 = sc.nextDouble();
-				System.out.println("Enter the second side length ---> ");
+				System.out.print("Enter the second side length ---> ");
 				length2 = sc.nextDouble();
-				System.out.print("Would you like to find the area or perimeter of the rectangle? ---> ");
-				do {
-					switch (sc.next().toUpperCase()) {
-					case "AREA":
-					case "AR":
-					case "A":
-						System.out.println("Area of rectangle = " + calc.rectArea(length1, length2) + "\n");
-						Thread.sleep(2000);
-						shouldRepeat = false;
-						break;
-					case "PERIMETER":
-					case "PER":
-					case "P":
-						System.out.println("Perimeter of rectangle = " + calc.rectPerimeter(length1, length2) + "\n");
-						Thread.sleep(2000);
-						shouldRepeat = false;
-						break;
-					default:
-						System.out.println("I'm sorry, that wasn't an option! Please try again.\n");
-						Thread.sleep(1000);
-						shouldRepeat = true;
-						break;
-					}
-				} while (shouldRepeat);
+				System.out.println("Area of rectangle = " + calc.rectArea(length1, length2) + 
+						", Perimeter of rectangle = " + calc.rectPerimeter(length1, length2) + "\n");
+				Thread.sleep(2000);
 				break;
 			case "CIRCLE":
 			case "CIRC":
 			case "CIR":
 				System.out.print("Enter the radius ---> ");
 				length1 = sc.nextDouble();
-				System.out.print("Would you like to find the area or circumference of the circle? ---> ");
-				do {
-					switch (sc.next().toUpperCase()) {
-					case "AREA":
-					case "AR":
-					case "A":
-						System.out.println("Area of circle = " + calc.circArea(length1) + "\n");
-						Thread.sleep(2000);
-						shouldRepeat = false;
-						break;
-					case "CIRCUMFERENCE":
-					case "CIRCUMF":
-					case "CIRCUM":
-					case "CIRC":
-						System.out.println("Circumference of circle = " + calc.circCircumf(length1) + "\n");
-						Thread.sleep(2000);
-						shouldRepeat = false;
-						break;
-					default:
-						System.out.println("I'm sorry, that wasn't an option! Please try again.\n");
-						Thread.sleep(1000);
-						shouldRepeat = true;
-						break;
-					}
-				} while (shouldRepeat);
+				System.out.println("Area of circle = " + calc.circArea(length1) + 
+						", Circumference of circle = " + calc.circCircumf(length1));
+				Thread.sleep(2000);
 				break;
 			case "LEAVE":
 			case "QUIT":
@@ -119,6 +55,7 @@ public class GeometryCalculatorMenu {
 			case "S":
 				System.out.println("Quitting now.");
 				sc.close();
+				shouldContinue = false;
 				break;
 			default:
 				System.out.println("I'm sorry, that wasn't an option! Please try again.\n");
