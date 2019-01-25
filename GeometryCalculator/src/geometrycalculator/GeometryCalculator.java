518ba5a86d259b16e1780ca4e1272e12e7ee55bc
Liad Hermelin <liad.hermelin@gmail.com>
2019-01-25 11:16:31 -0800

    added shapes to GeometryCalculator

--- GeometryCalculator/src/geometrycalculator/GeometryCalculator.java
+++ GeometryCalculator/src/geometrycalculator/GeometryCalculator.java
@@ -2,27 +2,55 @@ package geometrycalculator;
 
 public class GeometryCalculator {
 
-	public double squareArea(double length) {
-		return Math.round(length * length * 100.0) / 100.0;
+	public double squareArea(double l) {
+		return Math.round(l * l * 100.0) / 100.0;
 	}
 
-	public double squarePerimeter(double length) {
-		return Math.round(length * 400.0) / 100.0;
+	public double squarePerimeter(double l) {
+		return Math.round(l * 400.0) / 100.0;
 	}
 
-	public double rectArea(double length, double height) {
-		return Math.round(length * height * 100.0) / 100.0;
+	public double rectArea(double l, double h) {
+		return Math.round(l * h * 100.0) / 100.0;
 	}
 
-	public double rectPerimeter(double length, double height) {
-		return Math.round(length * 200.0 + height * 200.0) / 100.0;
+	public double rectPerimeter(double l, double h) {
+		return Math.round(l * 200.0 + h * 200.0) / 100.0;
 	}
 
-	public double circArea(double rad) {
-		return Math.round(100.0 * Math.PI * (rad * rad)) / 100.0;
+	public double circArea(double r) {
+		return Math.round(100.0 * Math.PI * (r * r)) / 100.0;
 	}
 
-	public double circCircumf(double rad) {
-		return Math.round(200.0 * Math.PI * rad) / 100.0;
+	public double circCircumf(double r) {
+		return Math.round(200.0 * Math.PI * r) / 100.0;
+	}
+
+	public double ellipseArea(double major, double minor) {
+		return Math.round(Math.PI * major * minor * 100.0) / 100.0;
+	}
+
+	public double triArea(double base, double h) {
+		return Math.round(base * h / 2.0 * 100.0) / 100.0;
+	}
+
+	public double trapArea(double a, double b, double h) {
+		return Math.round((a + b / 2.0) * h * 100.0) / 100.0;
+	}
+
+	public double cubeVolume(double l) {
+		return Math.round(l * l * l * 100.0) / 100.0;
+	}
+
+	public double cubeSA(double l) {
+		return Math.round(600.0 * (l * l)) / 100.0;
+	}
+	
+	public double rpVolume(double l, double w, double h) {
+		return Math.round(l * w * h * 100.0) / 100.0;
+	}
+	
+	public double rpSA(double l, double w, double h) {
+		return Math.round(200.0 * l * w + 200.0 * l * h + 200.0 * w * h) / 100.0;
 	}
 }
--- GeometryCalculator/src/geometrycalculator/GeometryCalculatorMenu.java
+++ GeometryCalculator/src/geometrycalculator/GeometryCalculatorMenu.java
@@ -12,7 +12,7 @@ public class GeometryCalculatorMenu {
 		double length1, length2, length3, length4, length5, length6, length7, length8;
 		boolean shouldContinue = true;
 		do {
-			System.out.print("Enter a shape! ---> ");
+			System.out.print("Enter a shape/command! ---> ");
 			String str = sc.next();
 
 			switch (str.toUpperCase()) {
@@ -21,8 +21,8 @@ public class GeometryCalculatorMenu {
 			case "SQ":
 				System.out.print("Enter a side length ---> ");
 				length1 = sc.nextDouble();
-				System.out.println("Area of square = " + calc.squareArea(length1) + 
-						", Perimeter of square = " + calc.squarePerimeter(length1) + "\n");
+				System.out.println("Area of square = " + calc.squareArea(length1) + ", Perimeter of square = "
+						+ calc.squarePerimeter(length1) + "\n");
 				Thread.sleep(1000);
 				break;
 			case "RECTANGLE":
@@ -31,8 +31,8 @@ public class GeometryCalculatorMenu {
 				length1 = sc.nextDouble();
 				System.out.print("Enter the second side length ---> ");
 				length2 = sc.nextDouble();
-				System.out.println("Area of rectangle = " + calc.rectArea(length1, length2) + 
-						", Perimeter of rectangle = " + calc.rectPerimeter(length1, length2) + "\n");
+				System.out.println("Area of rectangle = " + calc.rectArea(length1, length2)
+						+ ", Perimeter of rectangle = " + calc.rectPerimeter(length1, length2) + "\n");
 				Thread.sleep(1000);
 				break;
 			case "CIRCLE":
@@ -40,9 +40,90 @@ public class GeometryCalculatorMenu {
 			case "CIR":
 				System.out.print("Enter the radius ---> ");
 				length1 = sc.nextDouble();
-				System.out.println("Area of circle = " + calc.circArea(length1) + 
-						", Circumference of circle = " + calc.circCircumf(length1));
+				System.out.println("Area of circle = " + calc.circArea(length1) + ", Circumference of circle = "
+						+ calc.circCircumf(length1) + "\n");
 				Thread.sleep(1000);
+				break;
+			case "ELLIPSE":
+			case "ELLPSE":
+			case "EllPS":
+			case "ELPS":
+				System.out.print("Enter the semimajor axis ---> ");
+				length1 = sc.nextDouble();
+				System.out.print("Enter the semiminor axis ---> ");
+				length2 = sc.nextDouble();
+				System.out.println("Area of ellipse = " + calc.ellipseArea(length1, length2) + "\n");
+				Thread.sleep(1000);
+				break;
+			case "TRIANGLE":
+			case "TRINGLE":
+			case "TRI":
+			case "TR":
+				System.out.print("Enter the base ---> ");
+				length1 = sc.nextDouble();
+				System.out.print("Enter the height ---> ");
+				length2 = sc.nextDouble();
+				System.out.print("Would you also like to find the perimeter of the triangle? ---> ");
+				if (sc.next().equalsIgnoreCase("yes") || sc.next().equalsIgnoreCase("y")) {
+					System.out.print("Enter the base ---> ");
+					length3 = sc.nextDouble();
+					System.out.print("Enter the height ---> ");
+					length4 = sc.nextDouble();
+					System.out.print("Enter the height ---> ");
+					length5 = sc.nextDouble();
+					System.out.println("Area of triangle = " + calc.triArea(length1, length2)
+							+ " Perimeter of Triangle = " + (length3 + length4 + length5) + "\n");
+				} else
+					System.out.println("Area of triangle = " + calc.triArea(length1, length2) + "\n");
+				Thread.sleep(1000);
+				break;
+			case "PARALLELOGRAM":
+			case "PARALLEL":
+			case "PRLELGRM":
+			case "PARALEL":
+			case "PRLEL":
+				System.out.print("Enter the first side length ---> ");
+				length1 = sc.nextDouble();
+				System.out.print("Enter the second side length ---> ");
+				length2 = sc.nextDouble();
+				System.out.println("Area of parallelogram = " + calc.rectArea(length1, length2)
+						+ ", Perimeter of parallelogram = " + calc.rectPerimeter(length1, length2) + "\n");
+				Thread.sleep(1000);
+				break;
+			case "TRAPEZOID":
+			case "TRPZOID":
+			case "TRPZD":
+			case "TRPD":
+				System.out.print("Enter the length of the first base ---> ");
+				length1 = sc.nextDouble();
+				System.out.print("Enter the length of the second base ---> ");
+				length2 = sc.nextDouble();
+				System.out.print("Enter the height of the trapezoid ---> ");
+				length3 = sc.nextDouble();
+				System.out.println("Area of trapezoid = " + calc.trapArea(length1, length2, length3) + "\n");
+				break;
+			case "CUBE":
+			case "CBE":
+			case "CB":
+				System.out.print("Enter a side length ---> ");
+				length1 = sc.nextDouble();
+				System.out.println("Volume of cube = " + calc.cubeVolume(length1) + ", Surface Area of square = "
+						+ calc.cubeSA(length1) + "\n");
+				Thread.sleep(1000);
+				break;
+			case "RECTANGULAR PRISM":
+			case "RECT PRSM":
+			case "RECTPRSM":
+			case "RCTPRSM":
+			case "RP":
+				System.out.print("Enter the length of the prism ---> ");
+				length1 = sc.nextDouble();
+				System.out.print("Enter the width of the prism  ---> ");
+				length2 = sc.nextDouble();
+				System.out.print("Enter the height of the prism ---> ");
+				length3 = sc.nextDouble();
+				System.out.println("Volume of prism = " + calc.rpVolume(length1, length2, length3)
+						+ " Surface Area of prism = " + calc.rpSA(length1, length2, length3) + "\n");
 				break;
 			case "LEAVE":
 			case "QUIT":
